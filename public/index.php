971d2120113dfeb7b03c0f8754ca16f779050150
MFDW <dave.wastall@mobilefun.co.uk>
2016-03-11 17:10:36 +0000

    tidied things up

--- public/index.php
+++ public/index.php
@@ -1,14 +1,12 @@
 <?php
 
-// public/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// ... definitions
-
+// define route for /
 $app->get('/', function() use($app) {
 	return 'Hello Developers\' Academy';
-});
+}); 
 
 $app->run();
