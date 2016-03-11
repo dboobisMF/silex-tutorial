18ef866e17c43cff09ef5ea2a720f18bfd6fee1b
Dave Boobis <david.boobis@mobilefun.co.uk>
2016-03-11 16:46:57 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
+<?php
+
 // public/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 // ... definitions
+
+$app->get('/', function() use($app) {
+	return 'Hello Developers\' Academy';
+});
 
 $app->run();
