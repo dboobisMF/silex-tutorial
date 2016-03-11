8dc98050098bf12a33727c282da5e0e2edd7b9fb
I <anthony.cook@mobilefun.co.uk>
2016-03-11 18:07:24 +0000

    Annotate Controller and View

--- public/index.php
+++ public/index.php
@@ -5,14 +5,22 @@ require_once __DIR__ . '/../models/Task.php';
 
 $app = new Silex\Application();
 
+// switch on debug mode to make it easier to see what is going on in the browser
+$app['debug'] = true;
+
 // define route for /
 $app->get('/', function() use($app) {
+
 	// creates a new object instance of Task and puts it into $task
 	$task = new Task();
+
 	// get the message from the Task object
 	$message = $task->getMessage();
+
+	// display the View
 	require_once (__DIR__ . '/../views/index.phtml');
-	return true;
+
+	return "";
 }); 
 
 $app->run();
