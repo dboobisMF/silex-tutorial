29501ef60374fb6da8e0c98e81652ca52eaecf4d
I <anthony.cook@mobilefun.co.uk>
2016-03-11 18:06:41 +0000

    Annotated Task Model

--- models/Task.php
+++ models/Task.php
@@ -1,9 +1,11 @@
 <?php
-
+// this is a Model
 class Task
 {
+	// This is a private member variable
 	private $message='Hello Developers';
 
+	// This is a simple class method to return the value of $message
 	public function getMessage() {
 		return $this->message;
 	}
